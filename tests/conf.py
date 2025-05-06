eb81d346470d110ee1749e3e31bd587360d51cc0
Jaroslav Henner <1187265+jarovo@users.noreply.github.com>
2025-05-06 23:34:31 +0200

    Add integration tests.

---
+++ tests/conf.py
@@ -0,0 +1,8 @@
+from os import environ
+
+FAKTUROID_SLUG=environ['FAKTUROID_SLUG']
+FAKTUROID_CLIENT_ID=environ['FAKTUROID_CLIENT_ID']
+FAKTUROID_CLIENT_SECRET=environ['FAKTUROID_CLIENT_SECRET']
+
+TESTS_USER_AGENT = 'python-fakturoid-v3-tests (https://github.com/jarovo/python-fakturoid-v3)'
+TESTS_OBJECTS_NAME_PREFIX='test'

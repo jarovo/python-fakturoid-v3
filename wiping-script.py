2b5e7ab80cc8866a9f39d1ba2f87105ae0d2a933
Jaroslav Henner <1187265+jarovo@users.noreply.github.com>
2025-05-27 08:05:09 +0200

    Improve reliability of wipening script.
    Signed-off-by: Jaroslav Henner <1187265+jarovo@users.noreply.github.com>

--- wiping-script.py
+++ wiping-script.py
@@ -1,15 +1,35 @@
 import fakturoid
 from os import environ
+from fakturoid.models import InvoiceAction
 
-fakturoid_slug = environ["FAKTUROID_SLUG"]
-assert (
-    environ.get("ALLOW_WIPENING") == fakturoid_slug
-), f"ALLOW_WIPENING needs to be defined to allow wipening {fakturoid_slug}"
 
+def confirm_wipening_allowed():
+    fakturoid_slug = environ["FAKTUROID_SLUG"]
+    assert (
+        environ.get("ALLOW_WIPENING") == fakturoid_slug
+    ), f"ALLOW_WIPENING needs to be defined to allow wipening {fakturoid_slug}"
+
+
+# Dont proceed without safety check.
+confirm_wipening_allowed()
 fa = fakturoid.Fakturoid.from_env()
 
-subjects_to_delete = {subject.id: subject.name for subject in fa.subjects.list()}
-print(f"Deleting {subjects_to_delete}")
-for id in subjects_to_delete:
-    assert id
-    fa.subjects.delete(id)
+
+def delete_invoices():
+    invoices_to_delete = list(fa.invoices.list())
+    print(f"Deleting {invoices_to_delete}")
+    for invoice in invoices_to_delete:
+        if invoice.locked_at:
+            fa.invoice_action.fire(invoice.id, InvoiceAction.Unlock)
+        fa.invoices.delete(invoice.id)
+
+
+def delete_subjects():
+    subjects_to_delete = list(fa.subjects.list())
+    print(f"Deleting {subjects_to_delete}")
+    for subject in subjects_to_delete:
+        fa.subjects.delete(subject.id)
+
+
+delete_invoices()
+delete_subjects()
